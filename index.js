8ed9708601e504e80f6cdc4313651f48434c8989
Mac <victormaduka7@gmail.com>
2025-03-12 15:37:10 +0100

    fiexed index

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const http = require("http");
 const express = require("express");
-const db = require("./config/db"); // Database configuration and connection setup
+const db = require("./config/db");
 const OrderRoute = require("./routes/OrdersRoutes");
 
+const app = express();
+app.use(express.json());
+app.use("/api/orders", OrderRoute);
+
 db()
-  .then(() => {
-    console.log("Connected to MongoDB");
-    const app = express();
-    const server = http.createServer(app);
-    app.use(express.json());
-    app.use("/api/orders", OrderRoute);
-    server.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
-  });
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
+
+// Export the Express app for Vercel
+module.exports = app;
