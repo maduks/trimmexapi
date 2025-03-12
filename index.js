257844fa7b933c06f3e4e4e5dd901950d03edf33
Mac <victormaduka7@gmail.com>
2025-03-12 16:56:21 +0100

    Added cors

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const UserRoute = require("./routes UsersRoutes");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(cors());
+
 const app = express();
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+app.use(cors());
 app.use(express.json());
 app.use("/api/orders", OrderRoute);
 app.use("/api/users", UserRoute);
