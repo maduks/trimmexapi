3696d92803653cb9289ce58da78fb2be5e5628f8
Mac <victormaduka7@gmail.com>
2025-03-13 12:12:51 +0100

    Second push

--- index.js
+++ index.js
@@ -7,31 +7,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 app.use(bodyParser.json());
-
-const corsOptions = {
-  // Replace with your domain
-  origin: "https://trimmex.vercel.app",
-  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-
-  // Enable this if you need to
-  // send cookies or HTTP authentication
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://trimmex.vercel.app"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(express.json());
 app.use("/api/orders", cors(), OrderRoute);
